b2c680d24cd1e5a5ad602e92a34ebe5dd21c4184
nhan nguyen <nhan nguyen@DESKTOP-QRDST9N>
2020-08-19 11:22:24 +0700

    json example weather

---
+++ MyWeatherApplication/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MyWeatherApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using static MyWeatherApplication.Model.GetWeatherData;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MyWeatherApplication
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        //mot su kien goi ham async thi phai goi mot ham async
+        private async  void Button_Click(object sender, RoutedEventArgs e)
+        {
+           RootObject myWeather = await GetWeatherData.getOpenWeather();
+            string icon = string.Format("http://openweathermap.org/img/w/{0}.png", myWeather.weather[0].icon);//ket qua tra ve no chi la mot string khong phai anh
+           ResultIconImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+            ResultWeatherTextBlock.Text = myWeather.name + "-" + ((int)myWeather.main.temp).ToString();
+
+        }
+    }
+}
